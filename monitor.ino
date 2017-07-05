f8dbd68fc8d10079049d4a441848eaacd95d765b
Malomalomalomalo <lukegretta@gmail.com>
2017-07-05 15:24:14 -0500

    Update
    Updated Version

--- monitor.ino
+++ monitor.ino
@@ -1,9 +1,10 @@
 //Nessesary libraries for sensors
 #include <SimpleDHT.h>
+#include <SoftwareSerial.h>
 #include <EEPROM.h>
 #define pinDHT11 2
 #define MAXP 6
-#define BUFFSIZE 4
+#define BUFFSIZE 3000
 
 SimpleDHT11 dht11;
 byte temps[MAXP];
@@ -11,30 +12,30 @@ byte humid[MAXP];
 int buff;
 int tail;
 int head;
-int req;
+int req=0;
 
-void printTemps();
+SoftwareSerial btooth(3,4);
 
-//
 void setup() {
-    Serial.begin(9600);
+    btooth.begin(9600);
+   // Serial.begin(9600);
     //attachInterrupt(digitalPinToInterrupt(button), printTemps, CHANGE);
     //reads from EEPROM
     head=(EEPROM.read(MAXP<<1))*2*2*2*2*2*2*2*2+EEPROM.read(MAXP<<1|1);
     tail=(EEPROM.read((MAXP<<1)+2))*2*2*2*2*2*2*2*2+EEPROM.read((MAXP<<1)+3);
-    debugPrintFromMemory();
+  //  debugPrintFromMemory();
     buff=0;
 }
 
 //main loop
 void loop() {
-    Serial.println("Starting Loop");
+   // Serial.println("Starting Loop");
   //Writes temperature and humidity into RAM
     byte temperature = 0;
     byte humidity = 0;
     byte c=0;
     while(dht11.read(pinDHT11, &temperature, &humidity, NULL)&&c<51) {
-        Serial.println("Read DHT11 failed.");  
+       // Serial.println("Read DHT11 failed.");  
         c++;
     }
     c=0;
@@ -43,13 +44,13 @@ void loop() {
     humid[tail] = humidity;
   
     buff++;
-    Serial.print("buff: ");
-    Serial.println(buff); 
+   // Serial.print("buff: ");
+    //Serial.println(buff); 
     //Doesn't write outside of memory by restricting MAXP such that MAXP*2+4<=EEPROM size
     
     //Writes tempurature and humidity into memory
     if(buff==BUFFSIZE){
-      Serial.println("Starting Write To Memory");
+      //Serial.println("Starting Write To Memory");
        while(buff--){
           if(tail-buff>=0){
           EEPROM.update((tail-buff)<<1,temps[tail-buff]);
@@ -64,36 +65,50 @@ void loop() {
         EEPROM.update(MAXP<<1|1,(byte)(head));
         EEPROM.update((MAXP<<1)+2,(byte)(tail>>8));
         EEPROM.update((MAXP<<1)+3,(byte)(tail));
-        Serial.print("Unplug Now");
+       // Serial.print("Unplug Now");
      }
     
     
     //Checks for a query, if it exists it retrieves n recent ones
-    if(Serial.available()>0){
-    req=Serial.read();
+    if(btooth.available()>0){
+    req=btooth.read();
     }
-    while(req--){
+    while(req){
+      req--;
+     // Serial.println("Writing");
     if(tail-req>=0){
-    Serial.write(temps[tail-req]);
-    Serial.write(humid[tail-req]);
+    btooth.write(temps[tail-req]);
+    btooth.write(humid[tail-req]);
     }
     else{
-    Serial.write(temps[tail-req+MAXP]);
-    Serial.write(humid[tail-req+MAXP]);
+    btooth.write(temps[tail-req+MAXP]);
+    btooth.write(humid[tail-req+MAXP]);
     }
     }
 
     
-    Serial.print(head);
-    Serial.print(tail);
+  //  Serial.print(head);
+    //Serial.print(tail);
     
     //button interrupt
     delay(10000);
     tail=(tail+1)%MAXP;
     if(tail==head)head=(tail+1)%MAXP;
-    printTemps();
+    //printTemps();
 
 }
+void loadFromMemory(){
+int t=EEPROM.read((MAXP<<1)+2)*2*2*2*2*2*2*2*2+EEPROM.read((MAXP<<1)+3);
+    int r=EEPROM.read(MAXP<<1)*2*2*2*2*2*2*2*2+EEPROM.read(MAXP<<1|1);
+    int point=(head-1);
+    if(point==-1)point=MAXP-1;
+    do{
+    point=(point+1)%MAXP;
+    temps[point]=EEPROM.read(point<<1);
+    humid[point]=Serial.print((int)EEPROM.read(point<<1|1));
+    }while(point!=tail);
+}
+
 void debugPrintFromMemory(){
     int t=EEPROM.read((MAXP<<1)+2)*2*2*2*2*2*2*2*2+EEPROM.read((MAXP<<1)+3);
     int r=EEPROM.read(MAXP<<1)*2*2*2*2*2*2*2*2+EEPROM.read(MAXP<<1|1);
